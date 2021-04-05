dac02515074f16b27f0448fb9f9c13ebcbdcd4b1
Lemerald <Lemerald@github.com>
2021-04-05 17:44:57 +0200

    Projektdateien hinzufügen.

---
+++ CRCAssist/BeatCustomzier.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace CRCAssist
+{
+    partial class CRCAssist
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGp = new System.Windows.Forms.Label();
+            this.nudGp = new System.Windows.Forms.NumericUpDown();
+            this.rbExact = new System.Windows.Forms.RadioButton();
+            this.rbFast = new System.Windows.Forms.RadioButton();
+            this.rbEasy = new System.Windows.Forms.RadioButton();
+            this.gbDelay = new System.Windows.Forms.GroupBox();
+            this.cbInstant = new System.Windows.Forms.CheckBox();
+            this.cbTransition = new System.Windows.Forms.CheckBox();
+            this.nudLoops = new System.Windows.Forms.NumericUpDown();
+            this.lblLoops = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudGp)).BeginInit();
+            this.gbDelay.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLoops)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblGp
+            // 
+            this.lblGp.AutoSize = true;
+            this.lblGp.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblGp.Location = new System.Drawing.Point(17, 30);
+            this.lblGp.Name = "lblGp";
+            this.lblGp.Size = new System.Drawing.Size(132, 25);
+            this.lblGp.TabIndex = 0;
+            this.lblGp.Text = "Groundpunds:";
+            // 
+            // nudGp
+            // 
+            this.nudGp.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.nudGp.Location = new System.Drawing.Point(155, 32);
+            this.nudGp.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.nudGp.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudGp.Name = "nudGp";
+            this.nudGp.Size = new System.Drawing.Size(36, 27);
+            this.nudGp.TabIndex = 1;
+            this.nudGp.Value = new decimal(new int[] {
+            54,
+            0,
+            0,
+            0});
+            this.nudGp.ValueChanged += new System.EventHandler(this.nudGp_ValueChanged);
+            // 
+            // rbExact
+            // 
+            this.rbExact.AutoSize = true;
+            this.rbExact.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rbExact.Location = new System.Drawing.Point(6, 22);
+            this.rbExact.Name = "rbExact";
+            this.rbExact.Size = new System.Drawing.Size(54, 29);
+            this.rbExact.TabIndex = 3;
+            this.rbExact.Text = "1.8";
+            this.rbExact.UseVisualStyleBackColor = true;
+            this.rbExact.CheckedChanged += new System.EventHandler(this.rbExact_CheckedChanged);
+            // 
+            // rbFast
+            // 
+            this.rbFast.AutoSize = true;
+            this.rbFast.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rbFast.Location = new System.Drawing.Point(66, 22);
+            this.rbFast.Name = "rbFast";
+            this.rbFast.Size = new System.Drawing.Size(54, 29);
+            this.rbFast.TabIndex = 4;
+            this.rbFast.Text = "1.9";
+            this.rbFast.UseVisualStyleBackColor = true;
+            this.rbFast.CheckedChanged += new System.EventHandler(this.rbFast_CheckedChanged);
+            // 
+            // rbEasy
+            // 
+            this.rbEasy.AutoSize = true;
+            this.rbEasy.Checked = true;
+            this.rbEasy.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rbEasy.Location = new System.Drawing.Point(126, 22);
+            this.rbEasy.Name = "rbEasy";
+            this.rbEasy.Size = new System.Drawing.Size(54, 29);
+            this.rbEasy.TabIndex = 5;
+            this.rbEasy.TabStop = true;
+            this.rbEasy.Text = "2.0";
+            this.rbEasy.UseVisualStyleBackColor = true;
+            this.rbEasy.CheckedChanged += new System.EventHandler(this.rbEasy_CheckedChanged);
+            // 
+            // gbDelay
+            // 
+            this.gbDelay.Controls.Add(this.rbExact);
+            this.gbDelay.Controls.Add(this.rbEasy);
+            this.gbDelay.Controls.Add(this.rbFast);
+            this.gbDelay.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.gbDelay.Location = new System.Drawing.Point(17, 66);
+            this.gbDelay.Name = "gbDelay";
+            this.gbDelay.Size = new System.Drawing.Size(200, 53);
+            this.gbDelay.TabIndex = 6;
+            this.gbDelay.TabStop = false;
+            this.gbDelay.Text = "Delay in seconds:";
+            // 
+            // cbInstant
+            // 
+            this.cbInstant.AutoSize = true;
+            this.cbInstant.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cbInstant.Location = new System.Drawing.Point(268, 29);
+            this.cbInstant.Name = "cbInstant";
+            this.cbInstant.Size = new System.Drawing.Size(145, 29);
+            this.cbInstant.TabIndex = 8;
+            this.cbInstant.Text = "Start instantly";
+            this.cbInstant.UseVisualStyleBackColor = true;
+            this.cbInstant.CheckedChanged += new System.EventHandler(this.cbInstant_CheckedChanged);
+            // 
+            // cbTransition
+            // 
+            this.cbTransition.AutoSize = true;
+            this.cbTransition.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cbTransition.Location = new System.Drawing.Point(268, 64);
+            this.cbTransition.Name = "cbTransition";
+            this.cbTransition.Size = new System.Drawing.Size(233, 29);
+            this.cbTransition.TabIndex = 9;
+            this.cbTransition.Text = "Transition into CRC beat";
+            this.cbTransition.UseVisualStyleBackColor = true;
+            this.cbTransition.CheckedChanged += new System.EventHandler(this.cbTransition_CheckedChanged);
+            // 
+            // nudLoops
+            // 
+            this.nudLoops.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.nudLoops.Location = new System.Drawing.Point(434, 94);
+            this.nudLoops.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudLoops.Name = "nudLoops";
+            this.nudLoops.Size = new System.Drawing.Size(41, 27);
+            this.nudLoops.TabIndex = 10;
+            this.nudLoops.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudLoops.ValueChanged += new System.EventHandler(this.nudLoops_ValueChanged);
+            // 
+            // lblLoops
+            // 
+            this.lblLoops.AutoSize = true;
+            this.lblLoops.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLoops.Location = new System.Drawing.Point(268, 92);
+            this.lblLoops.Name = "lblLoops";
+            this.lblLoops.Size = new System.Drawing.Size(144, 25);
+            this.lblLoops.TabIndex = 11;
+            this.lblLoops.Text = "CRC beat loops:";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCreate.Location = new System.Drawing.Point(17, 138);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(484, 42);
+            this.btnCreate.TabIndex = 12;
+            this.btnCreate.Text = "Create beat";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(23, 187);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(57, 15);
+            this.lblOutput.TabIndex = 13;
+            this.lblOutput.Text = "Data here";
+            // 
+            // CRCAssist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 209);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.lblLoops);
+            this.Controls.Add(this.nudLoops);
+            this.Controls.Add(this.cbTransition);
+            this.Controls.Add(this.cbInstant);
+            this.Controls.Add(this.gbDelay);
+            this.Controls.Add(this.nudGp);
+            this.Controls.Add(this.lblGp);
+            this.Name = "CRCAssist";
+            this.Text = "CRCAssist";
+            ((System.ComponentModel.ISupportInitialize)(this.nudGp)).EndInit();
+            this.gbDelay.ResumeLayout(false);
+            this.gbDelay.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLoops)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label lblGp;
+        private System.Windows.Forms.NumericUpDown nudGp;
+        private System.Windows.Forms.RadioButton rbExact;
+        private System.Windows.Forms.RadioButton rbFast;
+        private System.Windows.Forms.RadioButton rbEasy;
+        private System.Windows.Forms.GroupBox gbDelay;
+        private System.Windows.Forms.CheckBox cbInstant;
+        private System.Windows.Forms.CheckBox cbTransition;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label lblLoops;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.NumericUpDown nudLoops;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ CRCAssist/BeatCustomzier.cs
@@ -0,0 +1,221 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRCAssist
+{
+    public partial class CRCAssist : Form
+    {
+
+        //Vars with getters and setters
+        private int gps;
+
+        private int loops;
+
+        private float beatDelay;
+
+        private bool startInstantly;
+
+        private bool transition;
+
+        public String debug = "debug";
+
+        public CRCAssist()
+        {
+            InitializeComponent();
+            Setgps(54);
+            Setloops(0);
+            SetbeatDelay(2.0f);
+            SetstartInstantly(false);
+            Settransition(false);
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            String output = Getgps() + "; " + GetbeatDelay() +"; " + GetstartInstantly() + "; " + Gettransition() + "; " + Getloops();
+            lblOutput.Text = output;
+            playSounds();
+        }
+
+        public void playSound(string file)
+        {
+            NAudio.Wave.WaveStream pcm = NAudio.Wave.WaveFormatConversionStream.CreatePcmStream(new NAudio.Wave.Mp3FileReader(file));
+            BlockAlignReductionStream stream = new NAudio.Wave.BlockAlignReductionStream(pcm);
+            DirectSoundOut output = new NAudio.Wave.DirectSoundOut();
+            output.Init(stream);
+            output.Play();
+            Sleep(((int)new AudioFileReader(file).TotalTime.TotalMilliseconds)-6);
+        }
+
+        public Queue<String> makePlaylist(string[] soundFiles)
+        {
+            //Easier access to each file
+            string crcP = soundFiles[0];
+            string oneGpEasyP = soundFiles[1];
+            string oneGpExactP = soundFiles[2];
+            string oneGpFastP = soundFiles[3];
+            string smashP = soundFiles[4];
+            string transitionP = soundFiles[5];
+            string warmupEasyP = soundFiles[6];
+            string warmupExactP = soundFiles[7];
+            string warmupFastP = soundFiles[8];
+
+            //Create a playlist
+            Queue<String> playlist = new Queue<string>();
+            //Select the correct gp beat speed
+            string oneGpP = oneGpEasyP;
+            string warmupP = warmupEasyP;
+            if (GetbeatDelay() == 1.8f) { oneGpP = oneGpExactP; warmupP = warmupExactP; }
+            else if (GetbeatDelay() == 1.9f) { oneGpP = oneGpFastP; warmupP = warmupFastP; }
+            //Fill playlist
+            //Only play the warmup if the checkbox isn't checked
+            if (!GetstartInstantly()) { playlist.Enqueue(warmupP); }
+            //Add the GP beat the requested amount of times
+            for (int i = Gettransition() ? 1 : 0; i < Getgps(); i++) { playlist.Enqueue(oneGpP); }
+            //Only necessary if the beat should transition into a CRC beat
+            if (Gettransition())
+            {
+                //Add the transition sound
+                playlist.Enqueue(transitionP);
+                //Add the amount of CRC loops
+                for (int i = 0; i < Getloops(); i++) { playlist.Enqueue(crcP); }
+                //Add a final snare sound;
+                playlist.Enqueue(smashP);
+            }
+
+            return playlist;
+        }
+
+        public void playSounds()
+        {
+            //Store files into soundFiles and sort them by name
+            string exePath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string soundPath = Path.GetFullPath(Path.Combine(exePath, @"..\..\..\..\..\soundFiles"));
+            string[] soundFiles = Directory.GetFiles(soundPath);
+            soundFiles.OrderBy(f => f);
+            Queue<String> playlist = makePlaylist(soundFiles);
+            while(playlist.Count > 0)
+            {
+                playSound(playlist.First());
+                playlist.Dequeue();
+            }
+            
+        }
+
+        //Accurate sleep method
+        public void Sleep(double milliseconds)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            while (stopwatch.ElapsedMilliseconds < milliseconds)
+            {
+                int timeout = (int)(milliseconds - stopwatch.ElapsedMilliseconds);
+                Thread.Sleep(timeout >= 0 ? timeout : 0);
+            }
+
+            stopwatch.Stop();
+        }
+
+        //Events
+        private void nudGp_ValueChanged(object sender, EventArgs e)
+        {
+            Setgps((int)nudGp.Value);
+        }
+
+        private void rbExact_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbExact.Checked) SetbeatDelay(1.8f);
+        }
+
+        private void rbFast_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbFast.Checked) SetbeatDelay(1.9f);
+        }
+
+        private void rbEasy_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbEasy.Checked) SetbeatDelay(2.0f);
+        }
+
+        private void cbInstant_CheckedChanged(object sender, EventArgs e)
+        {
+            SetstartInstantly(cbInstant.Checked);
+        }
+
+        private void cbTransition_CheckedChanged(object sender, EventArgs e)
+        {
+            Settransition(cbTransition.Checked);
+        }
+
+        private void nudLoops_ValueChanged(object sender, EventArgs e)
+        {
+            Setloops(Gettransition() ? (int)nudLoops.Value : 0);
+        }
+
+
+
+
+        //Getter and Setter
+        public int Getgps()
+        {
+            return gps;
+        }
+
+        public void Setgps(int value)
+        {
+            gps = value;
+        }
+
+        private int Getloops()
+        {
+            return loops;
+        }
+
+        private void Setloops(int value)
+        {
+            loops = value;
+        }
+
+        private float GetbeatDelay()
+        {
+            return beatDelay;
+        }
+
+        private void SetbeatDelay(float value)
+        {
+            beatDelay = value;
+        }
+
+        private bool GetstartInstantly()
+        {
+            return startInstantly;
+        }
+
+        private void SetstartInstantly(bool value)
+        {
+            startInstantly = value;
+        }
+
+        private bool Gettransition()
+        {
+            return transition;
+        }
+
+        private void Settransition(bool value)
+        {
+            transition = value;
+        }
+    }
+}
